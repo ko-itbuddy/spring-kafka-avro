52a827ed02ebd8a428c578aebfe439f1777022d1
나평근 <skvudrms54@gmail.com>
2024-09-25 22:21:56 +0900

    feat: 공통 Response 객체 추가및 기본 GlobalExceptionHandler 추가

---
+++ src/main/java/com/example/springkafkaavro/common/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.example.springkafkaavro.common;
+
+import com.example.springkafkaavro.common.ui.dto.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
+    public ApiResponse<Object> bindException(BindException e) {
+        return ApiResponse.of(
+            HttpStatus.BAD_REQUEST,
+            e.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+            null
+        );
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BadRequest.class)
+    public ApiResponse<Object> badRequest(BadRequest e) {
+        return ApiResponse.of(
+            HttpStatus.BAD_REQUEST,
+            e.getMessage(),
+            null
+        );
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ApiResponse<Object> internalException(Exception e) {
+        e.printStackTrace();
+        log.error(e.getMessage());
+        return ApiResponse.of(
+            HttpStatus.INTERNAL_SERVER_ERROR,
+            null
+        );
+    }
+
+}
---
+++ src/main/java/com/example/springkafkaavro/common/ui/dto/ApiResponse.java
@@ -0,0 +1,39 @@
+package com.example.springkafkaavro.common.ui.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Schema(description = "모든 응답의 기본객체")
+public class ApiResponse<T> {
+
+    @Schema(description = "응답 코드")
+    private int code;
+    @Schema(description = "응답 상태")
+    private HttpStatus status;
+    @Schema(description = "응답 메시지")
+    private String msg;
+    @Schema(description = "응답 결과")
+    private T result;
+
+    public ApiResponse(HttpStatus status, String msg, T result){
+        this.code = status.value();
+        this.status = status;
+        this.msg = msg;
+        this.result = result;
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus httpStatus, String msg, T result){
+        return new ApiResponse<T>(httpStatus, msg, result);
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus httpStatus, T result) {
+        return of(httpStatus, httpStatus.name(), result);
+    }
+
+    public static <T> ApiResponse<T> ok(T result) {
+        return of(HttpStatus.OK, result);
+    }
+
+}
